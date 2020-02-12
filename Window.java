dbf17b150fb122d08bfb810b95f4aa97fe4f3fce
Herstun <60628220+Herstun@users.noreply.github.com>
2020-02-12 17:29:40 -0500

    Update Window.java

--- Window.java
+++ Window.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package TylerRoux;
 
-/**
- *
- * @author Rouxk
- */
 import java.awt.Canvas;
 
 import javax.swing.JFrame;
 
 public class Window extends Canvas{
-
-	private static final long serialVersionUID = 6722047671872244215L;
 	private JFrame frame;
 	
 	public Window(int width, int height, String title, Game game){
