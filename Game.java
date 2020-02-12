772cfc58a8354673af1a4a884a721f609bdc3ae4
Herstun <60628220+Herstun@users.noreply.github.com>
2020-02-12 16:41:58 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,24 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package TylerRoux;
 
-/**
- *
- * @author Rouxk
- */
 import TylerGame.StandardHandler;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferStrategy;
-
 /**
  *
- * @author tjroux
+ * 
  */
 public class Game extends Canvas implements Runnable{
 
