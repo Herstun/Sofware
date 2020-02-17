d3de2b47c02776e106b6413b3026f32cb63e565e
THEFrog1 <50501217+THEFrog1@users.noreply.github.com>
2020-02-17 17:03:44 -0500

    Update Ball.java

--- Ball.java
+++ Ball.java
@@ -1,45 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package TylerRoux;
 
-/**
- *
- * @author Rouxk
- */
 import java.awt.Color;
 import java.awt.Graphics2D;
 import javax.swing.JOptionPane;
-
 import TylerGame.StandardGameObject;
 import TylerGame.StandardID;
 import TylerGame.StandardHandler;
 
-
-
-/**
- *
- * @author tjroux
- */
 public class Ball extends StandardGameObject{
 
-	public Ball(double x, double y, StandardHandler sh){
+	public Ball(double _x, double _y, StandardHandler sh){
 		super(x, y, StandardID.Ball);
-		
 		sh.addEntity(this);
-		
 		this.width = 15;
 		this.height = 15;
-		
 		this.velX = 5;
 		this.velY = -5;
 	}
 
 	@Override
 	public void tick() {
-		
 		if(this.x < 0 || this.x >= Game.window.width() - this.width){
 			this.velX = -this.velX;
 		}
@@ -54,15 +34,8 @@ public class Ball extends StandardGameObject{
 			System.exit(0);
 		}
 		
-		
 		this.x += this.velX;
 		this.y += this.velY;	
-	}
-
-	@Override
-	public void render(Graphics2D g2) {
-		g2.setColor(Color.blue);
-		g2.fillOval((int) x, (int) y, (int) this.width, (int) this.height);	
 	}
 }
 
