80a5d1702efabf1d181ac6b576b368c1e2fe742d
jeridmei13 <47527371+jeridmei13@users.noreply.github.com>
2020-02-17 16:55:38 -0500

    Update Window.java

--- Window.java
+++ Window.java
@@ -1,15 +1,11 @@
 
 package TylerRoux;
-
 import java.awt.Canvas;
-
 import javax.swing.JFrame;
-
 public class Window extends Canvas{
 	private JFrame frame;
-	
+
 	public Window(int width, int height, String title, Game game){
-		
 		this.frame = new JFrame(title);
 		this.frame.setSize(width, height);
 		this.frame.setDefaultCloseOperation(3);
@@ -18,11 +14,9 @@ public class Window extends Canvas{
 		this.frame.add(game);
 		this.frame.setVisible(true);
 	}
-	
 	public int width(){
 		return this.frame.getWidth();
 	}
-
 	public int height(){
 		return this.frame.getHeight();
 	}
