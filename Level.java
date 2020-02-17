1547b7724db9d737f97e408da881ffb8af5d2e41
jeridmei13 <47527371+jeridmei13@users.noreply.github.com>
2020-02-17 16:59:08 -0500

    Update Level.java

--- Level.java
+++ Level.java
@@ -11,39 +11,26 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Level {
-
 	private Scanner file = null;
 	public int brickCount = 0;
-	
 	public Level(String level, StandardHandler sh){	
 		int x = 0; 
 		int y = 0;
-		
-               
                 try{
-                    
 			this.file = new Scanner(new File(level));
 		while(this.file.hasNext()){
-	
-                
 		for(int i = 0; i < 8; i++){
-			
-			x = 0;
-			y += 55;
-			
+			this.x = 0;
+			this.y += 55;
 			String line = this.file.nextLine();
-			
 			for(int j = 0; j < 8; j++){
-				
-				x += 80;
-				
+				this.x += 80;
 				if(Integer.parseInt(line.substring(j, j+1)) != 0){
 					sh.addEntity(new Brick(x, y, Integer.parseInt(line.substring(j, j+1))));
 					this.brickCount++;
 				}
                         }
                 }	
-			
                 }
 		}catch(IOException e){
 			e.printStackTrace();
@@ -53,11 +40,8 @@ public class Level {
 			System.err.println("FILE IS NULL");
 			System.exit(0);
 		}
-
 	}
-
     void reload() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-        }
-
+}
