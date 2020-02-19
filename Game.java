89650ed44de7581116012bca1963e7027232ae5c
THEFrog1 <50501217+THEFrog1@users.noreply.github.com>
2020-02-19 15:44:15 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,3 +1,4 @@
+package TylerRoux;
 
 import TylerGame.StandardHandler;
 import java.awt.Canvas;
@@ -16,60 +17,58 @@ public class Game extends Canvas implements Runnable{
 	public static int score = 0;
 	private Font font;
      
-	public Game(int width, int height){
-		Game.window = new Window(width, height, "Bricks Be Gone", this);
-		this.sh = new StandardHandler();
-		this.paddle = new Paddle(300, 700, this.sh);
-		new Ball(380, 180, this.sh);
-		new Level("Resources/Levels/level1.txt", this.sh);
-		this.font = new Font("Arial", Font.TRUETYPE_FONT, 30);
-		this.addKeyListener(paddle);
-		this.start();
+public Game(int width, int height){
+	Game.window = new Window(width, height, "Bricks Be Gone", this);
+	this.sh = new StandardHandler();
+	this.paddle = new Paddle(300, 700, this.sh);
+	new Ball(380, 180, this.sh);
+	new Level("Resources/Levels/level1.txt", this.sh);
+	this.font = new Font("Arial", Font.TRUETYPE_FONT, 30);
+	this.addKeyListener(paddle);
+	this.start();
 	}
 	
-	private synchronized void start(){
-		if(running) return;
-		else{
-			this.running = true;
-			this.thread = new Thread(this);
-			this.thread.start();
-		}
+private synchronized void start(){
+	if(running) return;
+	else{
+		this.running = true;
+		this.thread = new Thread(this);
+		this.thread.start();
 	}
+}
 	
-	private synchronized void stop(){
-		if(!running) return;
-		try{
-			this.thread.join();
-		}catch(Exception e){
+private synchronized void stop(){
+	if(!running) return;
+	try{
+		this.thread.join();
+	}catch(Exception e){
+		e.printStackTrace();
+	}
+		
+	this.running = false;
+	System.exit(0);
+}
+
+public void run(){
+	this.requestFocus();
+		
+	while(running){
+		try {
+			TimeUnit.MILLISECONDS.sleep(20);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		this.running = false;
-		System.exit(0);
-	}
-
-	public void run(){
-		this.requestFocus();
-		
-		while(running){
-			try {
-				TimeUnit.MILLISECONDS.sleep(20);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			tick();
-			render();
-		}
-		
-		this.stop();
-	}
+		tick();
+		render();
+	}	
+	this.stop();
+}
 	
-	private void tick(){
-		this.sh.tick();	
-	}
-	
-	public static void main(String[] args) {
-		new Game(800, 800);
-	}
+private void tick(){
+	this.sh.tick();	
+}
+public static void main(String[] args) {
+	new Game(800, 800);
+}
 }
