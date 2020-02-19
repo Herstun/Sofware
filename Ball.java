e071dfa4fa9a172a463d2c8591f355e553c08586
THEFrog1 <50501217+THEFrog1@users.noreply.github.com>
2020-02-19 16:36:23 -0500

    Update Ball.java

--- Ball.java
+++ Ball.java
@@ -9,34 +9,31 @@ import TylerGame.StandardHandler;
 
 public class Ball extends StandardGameObject{
 
-	public Ball(double _x, double _y, StandardHandler sh){
-		super(x, y, StandardID.Ball);
-		sh.addEntity(this);
-		this.width = 15;
-		this.height = 15;
-		this.velX = 5;
-		this.velY = -5;
-	}
-
-	@Override
-	public void tick() {
-		if(this.x < 0 || this.x >= Game.window.width() - this.width){
-			this.velX = -this.velX;
-		}
-		
-		if(this.y < 0){
-			this.velY = -this.velY;
-		}
-		
-		if(this.y >= Game.window.height()){
-			
-			JOptionPane.showMessageDialog(null, "You lost! Your score was: "+Game.score);
-			System.exit(0);
-		}
-		
-		this.x += this.velX;
-		this.y += this.velY;	
-	}
+public Ball(double _x, double _y, StandardHandler sh){
+	super(x, y, StandardID.Ball);
+	sh.addEntity(this);
+	this.width = 15;
+	this.height = 15;
+	this.velX = 5;
+	this.velY = -5;
 }
 
-
+@Override
+public void tick() {
+	if(this.x < 0 || this.x >= Game.window.width() - this.width){
+		this.velX = -this.velX;
+	}
+		
+	if(this.y < 0){
+		this.velY = -this.velY;
+	}
+		
+	if(this.y >= Game.window.height()){	
+		JOptionPane.showMessageDialog(null, "You lost! Your score was: "+Game.score);
+		System.exit(0);
+		}
+		
+	this.x += this.velX;
+	this.y += this.velY;	
+	}
+}
