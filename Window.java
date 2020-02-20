4b316637b934e73b1c1dca0f5487aac0f2713384
Herstun <60628220+Herstun@users.noreply.github.com>
2020-02-20 12:35:44 -0500

    Update Window.java

--- Window.java
+++ Window.java
@@ -4,21 +4,28 @@ import java.awt.Canvas;
 import javax.swing.JFrame;
 
 public class Window extends Canvas{
-        private JFrame frame;
-        
-	public Window(int width, int height, String title, Game game){
-		this.frame = new JFrame(title);
-		this.frame.setSize(width, height);
-		this.frame.setDefaultCloseOperation(3);
-		this.frame.setResizable(false);
-		this.frame.setLocationRelativeTo(null);
-		this.frame.add(game);
-		this.frame.setVisible(true);
-	}
-	public int width(){
+    //2/20 Class creates a window that will get width and height, than builds the window after checking some parameters.
+    private JFrame frame;
+
+    protected static int defaultOperationVariable = 3;
+
+    public Window(int _width, int _height, String _title, Game _game){
+        //Creates the window from imputted width, height, title, and inputs the game into the window.
+	this.frame = new JFrame(_title);
+	this.frame.setSize(_width, _height);
+	this.frame.setDefaultCloseOperation(defaultOperationVariable);
+	this.frame.setResizable(false);
+	this.frame.setLocationRelativeTo(null);
+	this.frame.add(_game);
+	this.frame.setVisible(true);
+    }
+
+//======================GETTERS==========================
+    public int width(){
 		return this.frame.getWidth();
-	}
-	public int height(){
+    }
+
+    public int height(){
 		return this.frame.getHeight();
-	}
+    }
 }
