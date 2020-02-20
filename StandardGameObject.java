80627bde56ba16809316186822f8b6fb42bc57d9
Herstun <60628220+Herstun@users.noreply.github.com>
2020-02-20 13:21:59 -0500

    Update StandardGameObject.java

--- StandardGameObject.java
+++ StandardGameObject.java
@@ -7,23 +7,23 @@ import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
 public abstract class StandardGameObject {
-        protected double x;
-	protected double y;
-	protected double velX;
-	protected double velY;
-	protected int width;
-	protected int height;
-	protected StandardID id;
-	
-public StandardGameObject(double _x, double _y, StandardID id){
-	this.x = x;
-	this.y = y;
-	this.id = id;
-}
-	
-public abstract void tick();
-public abstract void render(Graphics2D g2);
-public Rectangle getBounds(){
-	return new Rectangle((int) _x,(int) _y,(int) width, (int) height);
-}
+    protected double x;
+    protected double y;
+    protected double velX;
+    protected double velY;
+    protected int width;
+    protected int height;
+    protected StandardID id;
+
+    public StandardGameObject(double _x, double _y, StandardID _id){
+	this.x = _x;
+	this.y = _y;
+	this.id = _id;
+    }
+
+    public abstract void tick();
+    public abstract void render(Graphics2D g2);
+    public Rectangle getBounds(){
+	return new Rectangle((int) x,(int) y,(int) width, (int) height);
+    }
 }
