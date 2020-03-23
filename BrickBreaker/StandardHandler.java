be7acbf57fd9285224bcf20d084809028d31e034
jeridmei13 <47527371+jeridmei13@users.noreply.github.com>
2020-03-23 13:02:05 -0400

    Update StandardHandler.java
    style guide changes

--- BrickBreaker/StandardHandler.java
+++ BrickBreaker/StandardHandler.java
@@ -1,123 +1,57 @@
 package BrickBreaker;
 
-import java.awt.Graphics2D;
-import java.util.ArrayList;
-import javax.swing.JOptionPane;
-
 /**
+ * This method is for the objects of the game and to declare the variables.
  *
- * @author tjroux
+ * @author Dymond, Tyler,Jerid, and Marquis; Last updated: 2/15/2020
  */
-public class StandardHandler {
 
-    private ArrayList<StandardGameObject> entities;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
 
-    public StandardHandler() {
-        this.entities = new ArrayList<StandardGameObject>();
+public abstract class StandardGameObject {
+    protected double _x;
+    protected double _y;
+    protected double velX;
+    protected double velY;
+    protected int width;
+    protected int height;
+    protected StandardID id;
+
+    /**
+     * This method declares the variables.
+     *
+     * @param _x Just declares the x variables for bounds.
+     * @param _y Just declares the y variables for bounds.
+     * @param _id Calls onto the standard id which calls on ball, brick, and paddle.
+     */
+    public StandardGameObject(double _x, double _y, StandardID _id) {
+        this._x = _x;
+        this._y = _y;
+        this.id = _id;
     }
 
-    public void tick() {
+    /**
+     * This method creates the abstract tick class for the whole game to move as
+     * one.
+     */
+    public abstract void tick();
 
-        if (this.countBricksAndDetermineWin()) {
-            //new Level("Resources/Levels/Level2.txt", this.sh);
-            JOptionPane.showMessageDialog(null, "Congrats, you won!");
-            System.exit(0);
-        }
-        /* if (brick == 0){
-                    Level level;
-                    level = new Level("Resources/Levels/level2.txt", this.sh);
-                    determine();
-                }*/
+    /**
+     * This method is the abstract render method that draws the canvas and
+     * objects onto the canvas.
+     *
+     * @param g2
+     */
+    public abstract void render(Graphics2D g2);
 
-        for (int i = 0; i < entities.size(); i++) {
-
-            //Grabs the player
-            if (this.entities.get(i).id == StandardID.Player) {
-
-                for (int j = 0; j < entities.size(); j++) {
-
-                    if (entities.get(j).id == StandardID.Ball) {
-
-                        //Collision between player & ball
-                        if (entities.get(i).getBounds().intersects(entities.get(j).getBounds())) {
-                            entities.get(j).velY = -entities.get(j).velY;
-                        }
-                    }
-                }
-            }
-
-            //Grabs the ball
-            if (entities.get(i).id == StandardID.Ball) {
-
-                for (int j = 0; j < entities.size(); j++) {
-
-                    if (entities.get(j).id == StandardID.Brick) {
-
-                        if (entities.get(i).getBounds().intersects(entities.get(j).getBounds())) {
-
-                            Game.score += 100;
-
-                            entities.remove(j);
-                            j--;
-
-                            entities.get(i).velY = -entities.get(i).velY;
-
-                        }
-                    }
-                }
-            }
-
-            this.entities.get(i).tick();
-
-        }
-    }
-
-    public void render(Graphics2D g2) {
-        for (int i = 0; i < entities.size(); i++) {
-            entities.get(i).render(g2);
-        }
-    }
-
-    public void addEntity(StandardGameObject obj) {
-        this.entities.add(obj);
-    }
-
-    public void removeEntity(StandardGameObject obj) {
-        for (int i = 0; i < entities.size(); i++) {
-            if (obj == entities.get(i)) {
-                entities.remove(i);
-                i--;
-            }
-        }
-    }
-
-    public void remove(StandardGameObject obj) {
-        this.entities.remove(obj);
-    }
-
-    //      int brick;
-    public boolean countBricksAndDetermineWin() {
-
-        int brick = 0;
-
-        for (int i = 0; i < entities.size(); i++) {
-            if (entities.get(i).id == StandardID.Brick) {
-                brick++;
-                // new Level("Resources/Levels/level2.txt", this.sh);
-            }
-            //   new Level("Resources/Levels/level2.txt", this.sh);
-        }
-        // new Level("Resources/Levels/level2.txt", this.sh);
-        return brick == 0;
-
+    /**
+     * This method is to make sure the canvas is inside the window and all
+     * variables work.
+     *
+     * @return
+     */
+    public Rectangle getBounds() {
+        return new Rectangle((int) _x, (int) _y, (int) width, (int) height);
     }
 }
-
-// private void Level() {
-//         Level level = new Level("Resources/Levels/level2.txt", this.sh);
-// }
-//  protected boolean determine() {
-//    return countBricksAndDetermineWin() == true;
-//}
-//}
-
