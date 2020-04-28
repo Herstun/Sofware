907b1045ffdfb9a14bbfbdf13786c1cd71cb1529
jeridmei13 <47527371+jeridmei13@users.noreply.github.com>
2020-04-28 14:42:50 -0400

    Update Game.java

--- BrickBreaker/Game.java
+++ BrickBreaker/Game.java
@@ -12,13 +12,16 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferStrategy;
 import java.util.concurrent.TimeUnit;
 
-public class Game extends Canvas implements Runnable {
+public final class Game extends Canvas implements Runnable {
 
     protected Thread thread;
     protected boolean running = false;
     public static Window window;
     protected StandardHandler sh;
     protected Paddle paddle;
+    protected Ball ball;
+    protected Boost boost;
+    protected Level level;
     public static int score = 0;
     protected Font font;
     private final int paddlePlacementWidth = 300;
@@ -27,8 +30,13 @@ public class Game extends Canvas implements Runnable {
     private final int ballPlacementHeight = 400;
     private final String fontType = "Arial";
     private final int fontSize = 30;
-    private static int gameSizeWidth = 800;
-    private static int gameSizeHeight = 800;
+    protected static final int gameSizeWidth = 800;
+    protected static final int gameSizeHeight = 800;
+    protected static int gameScoreIncrease = 100;
+    protected static final int boostPlacementWidth = 500;
+    protected static final int boostPlacementHeight = 500;
+    protected static int currentLevel = 1;
+    protected static boolean stopGameForLevelChange = false;
 
     public Game(int width, int height) {
         Game.window = new Window(width, height, "Bricks Be Gone", this);
@@ -37,15 +45,26 @@ public class Game extends Canvas implements Runnable {
 
         this.paddle = new Paddle(paddlePlacementWidth, paddlePlacementHeight, this.sh);
 
-        new Ball(ballPlacementWidth, ballPlacementHeight, this.sh);
+        this.ball = new Ball(ballPlacementWidth, ballPlacementHeight, this.sh);
 
-        new Level("src/Resources/Levels/Level1.txt", this.sh);
+        startLevel();
 
         this.font = new Font(fontType, Font.TRUETYPE_FONT, fontSize);
+
+        this.boost = new Boost(Game.boostPlacementWidth, Game.boostPlacementHeight, this.sh);
 
         this.addKeyListener(paddle);
 
         this.start();
+
+       /* if(StandardHandler.atlevel != 1 && Game.stopGameForLevelChange = true){
+            this.running = false;
+        }
+        if(StandardHandler.atlevel != 1 && Game.stopGameForLevelChange = false){
+        } else {
+            this.running = true;
+        }
+        */
     }
 
     private synchronized void start() {
@@ -57,7 +76,7 @@ public class Game extends Canvas implements Runnable {
         }
     }
 
-    private synchronized void stop() {
+    protected  synchronized void stop() {
         if (!running) {
             return;
         }
@@ -67,7 +86,7 @@ public class Game extends Canvas implements Runnable {
         }
 
         this.running = false;
-        System.exit(0);
+        //System.exit(0);
     }
 
     /**
@@ -91,6 +110,7 @@ public class Game extends Canvas implements Runnable {
 
     public void tick() {
         this.sh.tick();
+
     }
 
     public void render() {
@@ -103,19 +123,34 @@ public class Game extends Canvas implements Runnable {
 
         Graphics2D g2 = (Graphics2D) bs.getDrawGraphics();
 
-        g2.setColor(Color.BLACK);
+        g2.setColor(Color.WHITE);
         g2.fillRect(0, 0, Game.window.width(), Game.window.height());
         this.sh.render(g2);
 
-        g2.setColor(Color.WHITE);
+        g2.setColor(Color.BLACK);
         g2.setFont(this.font);
         g2.drawString("Score: " + Game.score, 20, 45);
 
         g2.dispose();
         bs.show();
     }
+    public void startLevel(){
+        this.level =new Level("src/Resources/Levels/Level2.txt", this.sh);
+    }
 
     public static void main(String[] args) {
-        new Game(gameSizeWidth, gameSizeHeight);
+         new Game(gameSizeWidth, gameSizeHeight);
     }
+
+    /**
+     *
+     */
+    /*
+    public static void levelPassStop(){
+        Game.stopGameForLevelChange =true;
+    }
+    public static void startAgain(){
+        Game.stopGameForLevelChange = false;
+    }
+*/
 }
