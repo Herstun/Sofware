36989c0abc9aba1213b62075cef5cc3e62551b06
jeridmei13 <47527371+jeridmei13@users.noreply.github.com>
2020-04-30 19:20:00 -0400

    Update Score.java

--- BrickBreaker/controller/Score.java
+++ BrickBreaker/controller/Score.java
@@ -1,31 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package BrickBreaker.controller;
 
+/**
+ * This gets the score from the translator and than prints the score onto the text file.
+ * @author Tyler 4/22/2020
+ */
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
-/**
- *
- * @author Rouxk
- */
 public class Score implements ScoreInterface{
+    private final String sourceOfScoreTextFile = "src/resources/Score/score.txt";
+    private final String messagePerScorePrint = "The score was:" //+_+ _score
+
     @Override
     public void writeScore(int _score) {
-   try {
-		FileWriter fw = new FileWriter("src/resources/Score/score.txt", true);
-        try (BufferedWriter bw = new BufferedWriter(fw)) {
-            if(_score > 0){
-                bw.write("The score was:" + " " + _score);
-                bw.newLine();
+        try {
+            FileWriter fw = new FileWriter(sourceOfScoreTextFile, true);
+            try (BufferedWriter bw = new BufferedWriter(fw)) {
+                if(_score > 0){
+                    bw.write(messagePerScorePrint + " " + _score);
+                    bw.newLine();
+                }
             }
         }
-       }catch (IOException e) {
+        catch (IOException e) {
             e.printStackTrace();
         }
-    } 
+    }
+    //=====================GETTTERS=============================
+    public String getSourceOfScoreTextFile() {
+        return sourceOfScoreTextFile;
+    }
+
+    public String getMessagePerScorePrint() {
+        return messagePerScorePrint;
+    }
 }
