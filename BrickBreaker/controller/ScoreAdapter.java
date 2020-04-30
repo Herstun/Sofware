0facc395f5bb2ac0d624241025019aefd17b4fed
jeridmei13 <47527371+jeridmei13@users.noreply.github.com>
2020-04-30 19:09:53 -0400

    Update ScoreAdapter.java

--- BrickBreaker/controller/ScoreAdapter.java
+++ BrickBreaker/controller/ScoreAdapter.java
@@ -1,16 +1,19 @@
 package BrickBreaker.controller;
 
 /**
- *
- * @author Rouxk
+ * Wrap between the Score Interface to the main game able to post a permanent record of the score.
+ * @author Tyler 4/20/2020
  */
+
 public class ScoreAdapter implements ScoreInterface{
      protected static final ScoreInterface scores = new Score();
-
+     //sends the score to the score class to be written into a text file.
     @Override
     public void writeScore(int _score) {
         scores.writeScore(_score);
     }
+    //============GETTERS=======================================================
+    public static ScoreInterface getScores() {
+        return scores;
+    }
 }
-    
-//}
