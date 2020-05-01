ac56df6139529a071354a4f2f4c3beb5a10eef24
jeridmei13 <47527371+jeridmei13@users.noreply.github.com>
2020-05-01 00:04:28 -0400

    Update BrickBreakerTest.java

--- BrickBreaker/TestPackage/BrickBreakerTest.java
+++ BrickBreaker/TestPackage/BrickBreakerTest.java
@@ -1,25 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package TestPackage;
-
 import BrickBreaker.view.Brick;
 
-/**
- *
- * @author meije
- */
 public class BrickBreakerTest {
-    private static final int intArray[] = new int[]{10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,};
+    private static final int testNumbers[] = new int[]{10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,};
     private static int testResult;
     public static void main(String[] args){
         testArrayOfOptions();
     }
     public static void  testArrayOfOptions(){
-        for(int i = 0; i< intArray.length; i++){
-            TestData.runTest(intArray[i], fromBricks(intArray[i]));
+        for(int i = 0; i< testNumbers.length; i++){
+            TestData.runTest(testNumbers[i], fromBricks(testNumbers[i]));
         }
     }
     public static int fromBricks(int i){
@@ -30,7 +19,7 @@ public class BrickBreakerTest {
     //======================================GETTERS===================================================
 
     public static int[] getIntArray() {
-        return intArray;
+        return testNumbers;
     }
 
     public static int getTestResult() {
