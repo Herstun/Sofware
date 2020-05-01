0113c1fc217093a953184bcc8de9f5e54e3fb491
jeridmei13 <47527371+jeridmei13@users.noreply.github.com>
2020-05-01 13:41:21 -0400

    Update BrickBreakerTest.java

--- BrickBreaker/TestPackage/BrickBreakerTest.java
+++ BrickBreaker/TestPackage/BrickBreakerTest.java
@@ -1,8 +1,15 @@
+package TestPackage;
+
+/**
+ * Gets all the variables and has an array of values we can use to test various amounts.
+ * @author Jerid 4/30/2020
+ */
 import BrickBreaker.view.Brick;
 
 public class BrickBreakerTest {
     private static final int testNumbers[] = new int[]{10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,};
     private static int testResult;
+    private static int amountOfBonus;
     public static void main(String[] args){
         testArrayOfOptions();
     }
@@ -13,13 +20,22 @@ public class BrickBreakerTest {
     }
     public static int fromBricks(int i){
         testResult = i*100;
-        testResult += Brick.ifComboBrickBroken( i);
+        amountOfBonus += Brick.ifComboBrickBroken( i);
+        testResult += amountOfBonus;
         return testResult;
     }
     //======================================GETTERS===================================================
 
     public static int[] getIntArray() {
         return testNumbers;
+    }
+
+    public static int[] getTestNumbers() {
+        return testNumbers;
+    }
+
+    public static int getAmountOfBonus() {
+        return amountOfBonus;
     }
 
     public static int getTestResult() {
@@ -29,5 +45,9 @@ public class BrickBreakerTest {
 
     public static void setTestResult(int testResult) {
         BrickBreakerTest.testResult = testResult;
+    }
+
+    public static void setAmountOfBonus(int amountOfBonus) {
+        BrickBreakerTest.amountOfBonus = amountOfBonus;
     }
 }
