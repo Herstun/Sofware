bb55790c90af2dd18bb787a8c11b1f26e250d05b
jeridmei13 <47527371+jeridmei13@users.noreply.github.com>
2020-04-30 23:37:21 -0400

    Update TestData.java

--- BrickBreaker/TestPackage/TestData.java
+++ BrickBreaker/TestPackage/TestData.java
@@ -11,9 +11,9 @@ package TestPackage;
  */
 public final class TestData {
     private int attack;
-    private int score;
+    private  int score;
     private int expected = 0;
-    private int bonusPointsMulti = 0;
+    protected int bonusPointsMulti = 2;
     private int bonusPoints=250;
     public TestData(int _attack, int _score) {
         this.attack = _attack;
@@ -23,13 +23,15 @@ public final class TestData {
 
      public static void runTest(int _attack, int _score) {
         TestData aTest = new TestData(_attack, _score);
+        System.out.println(aTest.toString());
     }
 
     public void calcTestRun() {
-        this.expected = this.attack*100;
+        this.expected += this.attack*100;
         this.bonusPointsMulti = this.attack/10;
         this.expected += (this.bonusPoints*this.bonusPointsMulti);
     }
+
     private String getResult() {
         if (this.expected == this.score) {
             return "PASS";
@@ -44,5 +46,47 @@ public final class TestData {
     @Override
     public String toString() {
         return this.getResult() + " the value of " + this.attack +" result in a score of " +this.score + " Expected: " + this.expected;
+    }
+    //=====================================================GETTERS==================================================
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getExpected() {
+        return expected;
+    }
+
+    public int getBonusPointsMulti() {
+        return bonusPointsMulti;
+    }
+
+    public int getBonusPoints() {
+        return bonusPoints;
+    }
+    //=======================================================SETTERS===================================================
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void setExpected(int expected) {
+        this.expected = expected;
+    }
+
+    public void setBonusPointsMulti(int bonusPointsMulti) {
+        this.bonusPointsMulti = bonusPointsMulti;
+    }
+
+    public void setBonusPoints(int bonusPoints) {
+        this.bonusPoints = bonusPoints;
     }
 }
