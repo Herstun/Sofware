6dde4835996abbe0ca7b2db4f2ef7d49ebad348a
jeridmei13 <47527371+jeridmei13@users.noreply.github.com>
2020-04-30 22:50:19 -0400

    Add files via upload

---
+++ BrickBreaker/TestPackage/BrickBreakerTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestPackage;
+
+/**
+ *
+ * @author meije
+ */
+public class BrickBreakerTest {
+    public static void main(String[] args){
+        testArrayOfOptions();
+    }
+    public static void  testArrayOfOptions(){
+        System.out.println("numbers :");
+        TestData.runTest(0, fromBricks());
+    }
+    public static int fromBricks(){
+        Brick.
+    }
+}
