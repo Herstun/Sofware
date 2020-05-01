a5883cca23c9470753c07c6f201e17d692022e3f
jeridmei13 <47527371+jeridmei13@users.noreply.github.com>
2020-04-30 23:37:55 -0400

    Update BrickBreakerTest.java

--- BrickBreaker/TestPackage/BrickBreakerTest.java
+++ BrickBreaker/TestPackage/BrickBreakerTest.java
@@ -5,19 +5,40 @@
  */
 package TestPackage;
 
+import BrickBreaker.view.Brick;
+
 /**
  *
  * @author meije
  */
 public class BrickBreakerTest {
+    private static final int intArray[] = new int[]{10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,};
+    private static int testResult;
     public static void main(String[] args){
         testArrayOfOptions();
     }
     public static void  testArrayOfOptions(){
-        System.out.println("numbers :");
-        TestData.runTest(0, fromBricks());
+        for(int i = 0; i< intArray.length; i++){
+            TestData.runTest(intArray[i], fromBricks(intArray[i]));
+        }
     }
-    public static int fromBricks(){
-        Brick.
+    public static int fromBricks(int i){
+        testResult = i*100;
+        testResult += Brick.ifComboBrickBroken( i);
+        return testResult;
+    }
+    //======================================GETTERS===================================================
+
+    public static int[] getIntArray() {
+        return intArray;
+    }
+
+    public static int getTestResult() {
+        return testResult;
+    }
+    //=========================================SETTERS==================================================
+
+    public static void setTestResult(int testResult) {
+        BrickBreakerTest.testResult = testResult;
     }
 }
